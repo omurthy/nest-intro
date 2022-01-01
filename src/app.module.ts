cf78940c535ae16980c7d10056650117ea63742f
omurthy <omuralcinthy@gmail.com>
2022-01-01 15:43:21 +0300

    Mongodb is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.lsyel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
