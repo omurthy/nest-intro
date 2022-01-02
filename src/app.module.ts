2c1f8cf16648f058fff508a67a8bfd2d0354b720
omurthy <omuralcinthy@gmail.com>
2022-01-02 15:24:57 +0300

    mongodb datasource updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.lsyel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://omur:12345@cluster0.lsyel.mongodb.net/nestjsDB?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
