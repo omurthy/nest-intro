02695f828c1ec9e51d705cdc6b5e3f986723aa1b
omurthy <omuralcinthy@gmail.com>
2022-01-08 00:11:39 +0300

    findCompany is added

--- src/companies/company.service.ts
+++ src/companies/company.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Company } from "./company.model";
@@ -8,6 +8,12 @@ import { Company } from "./company.model";
 export class CompanyService {
     constructor(@InjectModel('Company') private readonly companyModel: Model<Company>) { }
 
+    async insertCompany(name, phone, address, email) {
+        const company = new this.companyModel({ name: name, phone: phone, address: address, email: email });
+        const result = await company.save();
+        //console.log(result);
+        return result.id as string;
+    }
     async getAllCompanies() {
         const companies = await this.companyModel.find().exec();
         return companies.map(company => ({
@@ -19,10 +25,28 @@ export class CompanyService {
         }));
     }
 
-    async insertCompany(name, phone, address, email) {
-        const company = new this.companyModel({ name: name, phone: phone, address: address, email: email });
-        const result = await company.save();
-        //console.log(result);
-        return result.id as string;
+    async getSingleCompany(companyId: string) {
+        const company = await this.findCompany(companyId);
+    }
+    async updateCompany(companyId: string, compName: string, compPhone: string, compAddress: string, compEmail: string) {
+        const company = await this.findCompany(companyId);
+    }
+    async deleteCompany(companyId: string) {
+        const company = await this.findCompany(companyId);
+    }
+
+    async findCompany(companyId: string): Promise<Company> {
+
+        let company;
+        try {
+            company = await this.findCompany(companyId);
+        } catch (error) {
+            throw new NotFoundException("Error ! Company Not Found");
+        }
+        if (!company) {
+            throw new NotFoundException("Company can't found.Is your companyId correct ?");
+        }
+
+        return company;
     }
 }
