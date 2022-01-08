721018782a26c4f1de274a729605f763e3913388
omurthy <omuralcinthy@gmail.com>
2022-01-08 05:12:17 +0300

    customer crud is added in controller and service

--- src/customers/customer.controller.ts
+++ src/customers/customer.controller.ts
@@ -1,10 +1,52 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { CustomerService } from "./customer.service";
 
 @Controller("customers")
 export class CustomerController {
     constructor(private readonly customerService: CustomerService) { }
 
+    @Post()
+    async addCustomer(
+        @Body('name') custName: string,
+        @Body('phone') custPhone: number,
+        @Body('address') custAddress: string,
+        @Body('email') custEmail: string) {
 
+        const generatedId = await this.customerService.insertCustomer(custName, custPhone, custAddress, custEmail);
+
+        return { id: generatedId };
+
+    }
+
+
+    @Get()
+    async getAllCustomer() {
+        const customers = await this.customerService.getAllCustomer();
+        return customers;
+    }
+
+    @Get(":id")
+    async getCustomer(@Param('id') customerId: string) {
+        const customer = await this.customerService.getSingleCustomer(customerId);
+        return customer;
+    }
+
+    @Patch(":id")
+    async updateCustomer(@Param("id") custId: string,
+        @Body("name") custName: string,
+        @Body("phone") custPhone: number,
+        @Body("address") custAddress: string,
+        @Body('email') custEmail: string
+    ) {
+        const result = this.customerService.updateCustomer(custId, custName, custPhone, custAddress, custEmail);
+        return null;
+    }
+
+
+    @Delete(":id")
+    async removeCustomer(@Param('id') customerId: string) {
+        const customer = await this.customerService.deleteCustomer(customerId);
+        return null;
+    }
 
 }
--- src/customers/customer.service.ts
+++ src/customers/customer.service.ts
@@ -1,9 +1,70 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Customer } from "./customer.model";
 
 @Injectable()
 export class CustomerService {
+
     constructor(@InjectModel("Customer") private readonly customerModel: Model<Customer>) { }
+
+    async insertCustomer(customerName: string, customerPhone: number, customerAddress: string, customerEmail: string) {
+        const newCustomer = new this.customerModel({ name: customerName, phone: customerPhone, address: customerAddress, email: customerEmail });
+        const result = await newCustomer.save();
+
+        return result.id as string;
+    }
+
+    async getAllCustomer() {
+        const customers = await this.customerModel.find().exec();
+
+        return customers.map(customer => ({
+            id: customer.id,
+            name: customer.name,
+            phone: customer.phone,
+            address: customer.address,
+            email: customer.email
+        }))
+    }
+
+    async getSingleCustomer(customerId: string) {
+        const customer = this.findCustomer(customerId);
+        return customer;
+
+    }
+
+    async updateCustomer(customerId: string, customerName: string, customerPhone: number, customerAddress: string, customerEmail: string) {
+        const updatedCustomer = await this.findCustomer(customerId);
+        if (customerName) {
+            updatedCustomer.name = customerName;
+        }
+        if (customerPhone) {
+            updatedCustomer.phone = customerPhone;
+        }
+        if (customerAddress) {
+            updatedCustomer.address = customerAddress;
+        }
+        if (customerEmail) {
+            updatedCustomer.email = customerEmail;
+        }
+        updatedCustomer.save();
+
+    }
+
+    async findCustomer(customerId: string) {
+        let customer;
+        try {
+            customer = await this.customerModel.findById(customerId);
+        } catch (error) {
+            throw new NotFoundException("Error :Customer not found !");
+        }
+        if (!customer) {
+            throw new NotFoundException("Customer cant found");
+        }
+        return customer;
+    }
+    async deleteCustomer(customerId: string) {
+        const result = await this.customerModel.deleteOne({ id: customerId }).exec();
+        console.log(result);
+    }
 }
