0c4edcbe1667a6e9e71b07c53c52757efdb11c56
omurthy <omuralcinthy@gmail.com>
2022-01-08 23:48:54 +0300

    crud methods are added

--- src/clients/client.controller.ts
+++ src/clients/client.controller.ts
@@ -1,10 +1,44 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { ClientService } from "./client.service";
 
-@Controller()
+@Controller("clients")
 export class ClientController {
     constructor(private readonly clientService: ClientService) { }
 
+    @Post()
+    async insertClient(@Body('name') clientName: string,
+        @Body('companyId') companyId: string,
+        @Body('productId') productId: string
+    ) {
+        const generatedId = await this.clientService.insertClient(clientName, companyId, productId);
+        return { id: generatedId };
+    }
 
+    @Get()
+    async getAllClients() {
+        const clients = await this.clientService.getAllClients();
+        return clients;
+    }
 
+    @Get(":id")
+    async getSingleClients(@Param("id") clientId: string) {
+        const client = await this.clientService.getSingleClient(clientId);
+        return client;
+    }
+
+    @Patch(":id")
+    async updateClient(@Param("id") clientId: string,
+        @Body("name") clientName: string,
+        @Body("companyId") clientCompanyId: string,
+        @Body("productId") clientProductId: string
+    ) {
+        const result = await this.clientService.updateClient(clientId, clientName, clientCompanyId, clientProductId);
+        return null;
+    }
+
+    @Delete(":id")
+    async removeClient(@Param("id") clientId: string) {
+        await this.clientService.deleteClient(clientId);
+        return null;
+    }
 }
