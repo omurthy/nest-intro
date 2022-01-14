2eeb95063184ccda5adc6e5894b5b9f0b7fa9066
omurthy <omuralcinthy@gmail.com>
2022-01-15 02:29:35 +0300

    userModule is added

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserController } from "./user.controller";
+import { UserSchema } from "./user.model";
+import { UserService } from "./user.service";
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: []
+    imports: [MongooseModule.forFeature([
+        { name: "User", schema: UserSchema }
+    ])],
+    controllers: [UserController],
+    providers: [UserService]
 })
 export class UserModule { }
