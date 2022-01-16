b769d8c74d6025aec4851faeddfe9e60333d652f
omurthy <omuralcinthy@gmail.com>
2022-01-16 17:04:16 +0300

    user crud  services and controller is added

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller("ısers")
@@ -11,6 +11,32 @@ export class UserController {
     async getAllUsers() {
         const users = await this.userService.getUsers();
         return users;
+    }
+
+    @Get(":id")
+    async getUser(@Param("id") userId: string) {
+        const result = await this.userService.getSingleuser(userId);
+        return result;
+    }
+
+
+    @Post()
+    async saveUser(@Body("name") userName: string) {
+        const generatedId = await this.userService.insertUser(userName);
+        return { id: generatedId }
+    };
+
+    @Patch(":id")
+    async updateUser(@Param("id") userId: string,
+        @Body("name") userName: string) {
+        const result = await this.userService.updateUser(userId, userName);
+        return result;
+    }
+
+    @Get(":id")
+    async removeUser(@Param("id") userId: string) {
+        const user = this.userService.deleteUser(userId);
+        return null;
     }
 
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "./user.model";
@@ -16,4 +16,43 @@ export class UserService {
             name: user.name
         }))
     };
+
+    async getSingleuser(id: string) {
+        const result = await this.findUser(id);
+        return { id: result.id, name: result.name }
+    }
+    async insertUser(name: string) {
+        const newUser = new this.userModel({ name: name });
+        const result = await newUser.save();
+
+        return result.id as string;
+    }
+
+    async updateUser(id: string, userName: string) {
+        const foundedUser = await this.findUser(id);
+        if (userName)
+            foundedUser.name = userName;
+
+        foundedUser.save();
+    }
+
+    async deleteUser(id: string) {
+        const deleteUser = await this.userModel.deleteOne({ _id: id }).exec();
+        if (deleteUser.deletedCount == 0)
+            throw new NotFoundException("User not deleted");
+    }
+
+    async findUser(UserId: string) {
+        let result;
+        try {
+            result = await this.userModel.findById(UserId);
+        } catch (error) {
+            throw new NotFoundException("ERROR ! : Not Found User");
+        }
+
+        if (!result)
+            throw new NotFoundException("Not founded user");
+
+        return result;
+    }
 } 
