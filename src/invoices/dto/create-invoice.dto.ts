599b4381d4ce042d50e7b37889ea3972cbf3d1d5
omurthy <omuralcinthy@gmail.com>
2022-01-20 00:08:28 +0300

    create-invoice model is added

--- src/invoices/dto/create-invoice.dto.ts
+++ src/invoices/dto/create-invoice.dto.ts
@@ -1,1 +1,16 @@
-export class CreateInvoiceDto { }
+import * as mongoose from 'mongoose';
+
+export const InvoiceSchema = new mongoose.Schema({
+    productName: { type: String, required: true },
+    price: { type: Number, required: true },
+    customerId: { type: String, required: true },
+    customerName: { type: String, required: true },
+    discount: { type: Number, required: true },
+});
+export class CreateInvoiceDto extends mongoose.Document {
+    productName: string;
+    price: number;
+    customerId: string;
+    customerName: string;
+    discount: string;
+}
